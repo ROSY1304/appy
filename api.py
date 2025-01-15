52fbba727b33eeff4b3a16d6559ae4df977b8787
Rosa Isela Miranda Perez <rm007522@gmail.com>
2025-01-14 21:37:41 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, render_template
 import os
 import nbformat
 from flask_cors import CORS
